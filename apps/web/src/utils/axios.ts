60532ed28c9c2218da6d728b9514132285f441bc
connect-spesifickly <mozar.sosial@gmail.com>
2025-07-17 13:41:05 +0700

    make baseUrl dinamic

--- apps/web/src/utils/axios.ts
+++ apps/web/src/utils/axios.ts
@@ -1,4 +1,8 @@
 import axios from "axios";
+
+const baseURL =
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api";
+
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL,
 });
